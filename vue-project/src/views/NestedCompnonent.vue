c42c45afbc713c646dc968533ee353c2a142b4ca
Doreki <idh0325@naver.com>
2023-02-07 10:00:09 +0900

    컴포넌트 심화

---
+++ vue-project/src/views/NestedCompnonent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PageTitle />
+  </div>
+</template>
+
+<script>
+import PageTitle from "../components/PageTitle.vue";
+export default {
+  name: "GitNestedCompnonent",
+  components: { PageTitle },
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
