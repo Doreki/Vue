a2332dd2a54e7197aa1912551e466c73cdd34f97
Doreki <idh0325@naver.com>
2023-02-08 23:00:52 +0900

    데이터 바인딩

--- udemy/basics-01-starting-code/app.js
+++ udemy/basics-01-starting-code/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish this course and learn Vue!",
+      courseGoalA: "Finish this course and learn Vue!",
+      courseGoalB: "<h2>Master Vue and build amazing app</h2>",
       vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
