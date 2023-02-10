7fa30944d7cb6f2299f135271ded2d8072fbe1b5
Doreki <idh0325@naver.com>
2023-02-11 08:55:23 +0900

    슬롯

---
+++ udemy/section9/cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-prop="..."></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "VueCourseGoals",
+
+  data() {
+    return {
+      goals: ["Finish the course", "Learn the Vue"],
+    };
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
