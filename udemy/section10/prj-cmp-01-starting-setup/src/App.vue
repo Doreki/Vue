d9559a43a295d725837743fbeb80ca2c0e318546
Doreki <idh0325@naver.com>
2023-02-13 20:53:47 +0900

    학습 리소스 앱 프로젝트 진행중

---
+++ udemy/section10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resource :resources="storedResources"></stored-resource>
+</template>
+
+<script>
+import TheHeader from '../../../section9/cmp-adv-01-starting-setup/src/components/layout/TheHeader.vue';
+import StoredResource from './components/learning-resources/StoredResource.vue';
+
+export default {
+  name: 'VueApp',
+  components: {
+    StoredResource,
+    TheHeader,
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official-Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The Google documentation.',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
