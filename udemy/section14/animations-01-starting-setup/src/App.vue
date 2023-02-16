be543db5a75dc15442cc5acb8eca21ca35041ff9
Doreki <idh0325@naver.com>
2023-02-17 08:47:33 +0900

    modal css

--- udemy/section14/animations-01-starting-setup/src/App.vue
+++ udemy/section14/animations-01-starting-setup/src/App.vue
@@ -3,7 +3,14 @@
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition name="para">
+      <p v-if="paraIsVisible">This is only sometimes visible..</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -15,11 +22,18 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false, animatedBlock: false };
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+    };
   },
   methods: {
     showDialog() {
       this.dialogIsVisible = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
     hideDialog() {
       this.dialogIsVisible = false;
@@ -79,7 +93,35 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.v-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.para-enter-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
--- udemy/section14/animations-01-starting-setup/src/components/BaseModal.vue
+++ udemy/section14/animations-01-starting-setup/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -34,7 +37,7 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.3s ease-out forwards;
+  /* animation: modal 0.3s ease-out forwards; */
 }
 
 @keyframes modal {
@@ -47,5 +50,13 @@ dialog {
     opacity: 1;
     transform: translateY(0) scale(1);
   }
+}
+
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
+}
+
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
 }
 </style>
