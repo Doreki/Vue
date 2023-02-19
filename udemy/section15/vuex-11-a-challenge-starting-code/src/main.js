7ddb7b51ebf25254e13920ca52b10837cd4edc1a
Doreki <idh0325@naver.com>
2023-02-19 16:55:15 +0900

    assignment complete

--- udemy/section15/vuex-11-a-challenge-starting-code/src/main.js
+++ udemy/section15/vuex-11-a-challenge-starting-code/src/main.js
@@ -1,12 +1,18 @@
 import { createApp } from 'vue';
-
+import { createStore } from 'vuex';
 import router from './router.js';
 import App from './App.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 
-const app = createApp(App)
-
+const app = createApp(App);
+const store = createStore({
+  state() {
+    isLoggedIn: false,
+  },
+  mutations: {},
+});
 app.use(router);
+app.use(store);
 
 app.component('base-badge', BaseBadge);
 
