3a12a2a59a9290fd318654b62e6dee3fa3bb6b9f
Doreki <idh0325@naver.com>
2023-02-20 21:58:19 +0900

    vue slot

--- udemy/section16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ udemy/section16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,20 +1,43 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul>
-      LIST OF COACHES
-    </ul>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
+import BaseButton from '../../../../../section10/prj-cmp-01-starting-setup/src/components/UI/BaseButton.vue';
+import BaseCard from '../../../../../section10/prj-cmp-01-starting-setup/src/components/UI/BaseCard.vue';
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: { CoachItem, BaseCard, BaseButton },
   name: 'VueCoachesList',
 
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
   data() {
     return {};
   },
@@ -25,4 +48,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
