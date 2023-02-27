df2de2f39785bb14790bd4ce5cd3e3e3ab928825
Doreki <idh0325@naver.com>
2023-02-27 20:52:48 +0900

    api 통신

--- trello/src/components/Home.vue
+++ trello/src/components/Home.vue
@@ -3,6 +3,12 @@
     Home
     <div>
         Board List:
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <div v-for="b in boards" :key="b.id">
+            {{ b }}
+            </div>
+        </div>
         <ul>
             <router-link to="b/1">Board 1 </router-link> 
             <router-link to="b/2">Board 2 </router-link> 
@@ -12,7 +18,32 @@
 </template>
 
 <script>
-export default {};
+import { board } from "../api";
+
+export default {
+  data() {
+    return {
+      loading: false,
+      boards: []
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      board
+        .fetch()
+        .then(data => {
+          this.boards = data;
+        })
+        .finally(res => {
+          this.loading = false;
+        });
+    }
+  }
+};
 </script>
 
 <style >
